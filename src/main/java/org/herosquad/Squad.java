23e134b2a0f4bee8bee5a0e3af2e2dfe5ce7b1c5
c-okeyo <chrisokello26@gmail.com>
2023-05-21 22:32:10 +0300

    squad class, getters and setters created

---
+++ src/main/java/org/herosquad/Squad.java
@@ -0,0 +1,54 @@
+package org.herosquad;
+
+import org.herosquad.Hero;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Squad {
+    private int maxSize;
+    private String name;
+    private String cause;
+    private List<Hero> heroes;
+
+    public Squad(int maxSize, String name, String cause) {
+        this.maxSize = maxSize;
+        this.name = name;
+        this.cause = cause;
+        this.heroes = new ArrayList<>();
+    }
+
+    // Getters and setters
+
+    public int getMaxSize() {
+        return maxSize;
+    }
+
+    public void setMaxSize(int maxSize) {
+        this.maxSize = maxSize;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCause() {
+        return cause;
+    }
+
+    public void setCause(String cause) {
+        this.cause = cause;
+    }
+
+    public List<Hero> getHeroes() {
+        return heroes;
+    }
+
+    public void setHeroes(List<Hero> heroes) {
+        this.heroes = heroes;
+    }
+}
